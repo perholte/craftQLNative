76ca3b29441521b7a76e49b624571140d63d14c5
Per Holte <phholte@stud.ntnu.no>
2021-11-08 19:34:32 +0100

    #4 - started implementation of modals

--- App.tsx
+++ App.tsx
@@ -3,23 +3,25 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import CustomHeader from './components/header/Header';
+import BeerModal from './components/beerModal/BeerModal';
 
 export default function App() {
-  return (
-    <SafeAreaProvider>
-      <View style={styles.container}>
-        <CustomHeader />
-        <StatusBar style="auto" />
-      </View>
-    </SafeAreaProvider>
-  );
+	return (
+		<SafeAreaProvider>
+			<View style={styles.container}>
+				<CustomHeader />
+				<BeerModal />
+				<StatusBar style='auto' />
+			</View>
+		</SafeAreaProvider>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: 'rgba(232, 205, 102, 255)',
+		alignItems: 'center',
+		justifyContent: 'flex-start',
+	},
 });
