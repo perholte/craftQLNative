e26a0f7ed5a593f4818fcd69c63edd94f680b47b
André Schjøth <aschjoth@gmail.com>
2021-11-10 11:35:29 +0100

    #3 setup apollo client

--- App.tsx
+++ App.tsx
@@ -1,3 +1,9 @@
+import {
+	ApolloClient,
+	ApolloProvider,
+	createHttpLink,
+	InMemoryCache,
+} from '@apollo/client';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
@@ -5,21 +11,32 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import CustomHeader from './components/header/Header';
 
 export default function App() {
-  return (
-    <SafeAreaProvider>
-      <View style={styles.container}>
-        <CustomHeader />
-        <StatusBar style="auto" />
-      </View>
-    </SafeAreaProvider>
-  );
+	const link = createHttpLink({
+		uri: 'http://129.241.104.92:4000',
+	});
+
+	const client = new ApolloClient({
+		link,
+		cache: new InMemoryCache(),
+	});
+
+	return (
+		<ApolloProvider client={client}>
+			<SafeAreaProvider>
+				<View style={styles.container}>
+					<CustomHeader />
+					<StatusBar style='auto' />
+				</View>
+			</SafeAreaProvider>
+		</ApolloProvider>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'flex-start',
+	},
 });
